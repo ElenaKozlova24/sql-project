e7031ec1dcf3623a58cded43b4a6c605569e64fc
lena <4688104@mail.ru>
2024-08-06 14:46:17 +0300

    fix configurator path

--- src/main/java/configuration/db/Configurator.java
+++ src/main/java/configuration/db/Configurator.java
@@ -9,10 +9,9 @@ public class Configurator {
 
     private static final String DB_PROPERTIES_FILE = "/resources/dbproperties";
 
-
     public Properties getConfigurator() throws IDException, IOException {
         Properties properties = new Properties();
-        InputStream inputStream = new FileInputStream("/Users/kirill/IdeaProjects/sql3/src/resources/dbproperties");
+        InputStream inputStream = new FileInputStream( System.getProperty("user.dir") + "/src/resources/dbproperties");
         properties.load(inputStream);
         inputStream.close();
         return properties;
